7a4e12c9e6e0220739b5251d3bce9c60519f11e9
Vika <vihihika2005@gmail.com>
2026-09-23 15:26:33 +0300

    33

--- src/lab1/ex-04_minutes_to_hhmm.py
+++ src/lab1/ex-04_minutes_to_hhmm.py
@@ -1,8 +1,9 @@
 #задание 4
 
 minute=int(input('Минуты:'))
-ans=f'{minute//60}:{(minute%60)}'
-if (ans[-1]=='0' and ans[-2]==':'):
-    print(ans+'0')
+ch=str(minute//60)
+mi=str((minute%60))
+if int(mi)<10:
+    print(ch+':'+'0'+mi)
 else:
-    print(ans)
+    print(ch+':'+mi)
